608648c9714d17cda3b23431de1b5738906eef37
just1jp <jmendelson22@gmail.com>
2016-12-14 15:54:44 -0800

    Complete Pseudoclassical Stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.sizeVal = 0;
+  obj.storage = {};
+
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.sizeVal] = value;  
+    this.sizeVal++;
+  },
+  pop: function() {
+    if (this.sizeVal > 0) {
+      this.sizeVal--;
+      var removed = this.storage[this.sizeVal];
+      delete this.storage[this.sizeVal];     
+    }
+    return removed;  
+  },
+  size: function() {
+    return this.sizeVal;  
+  }
+
+};
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.sizeVal = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.sizeVal] = value;
+  this.sizeVal++;
+};
+
+Stack.prototype.pop = function(value) {
+  if (this.sizeVal > 0) {
+    this.sizeVal--;
+  }
+  var giveVal = this.storage[this.sizeVal];
+  delete this.storage[this.sizeVal];
+  return giveVal; 
+};
+
+Stack.prototype.size = function(value) {
+  return this.sizeVal;      
+};
 
