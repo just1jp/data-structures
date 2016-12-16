ca91bed3de3f2feae999aea5757a91f0e2814afa
just1jp <jmendelson22@gmail.com>
2016-12-15 16:42:53 -0800

    Added Work to graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,26 +2,39 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
+
+  //this.edges = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes[node] = {node: node, edges: []};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  for (var key in this.nodes) {
+    if (this.nodes.node === node) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.nodes[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+ 
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges.push([fromNode, toNode]);
 };
 
 // Remove an edge between any two specified (by value) nodes.
@@ -35,5 +48,16 @@ Graph.prototype.forEachNode = function(cb) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+
+
+
+
+
+
+
+
 
 
