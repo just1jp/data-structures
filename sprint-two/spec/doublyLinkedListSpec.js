fde8cc8a70475bc5c17d66c9a62981840a9f65b5
just1jp <jmendelson22@gmail.com>
2016-12-17 15:41:37 -0800

    Write out DLL findNode, addToEnd methods and spec file

--- sprint-two/spec/doublyLinkedListSpec.js
+++ sprint-two/spec/doublyLinkedListSpec.js
@@ -11,6 +11,7 @@ describe('Doubly Linked List', function() {
   });
 
   it('should have methods named "insertAtEnd", "insertBefore", "insertAfter", "addToHead", "removeTail", and "contains"', function() {
+    expect(doublyLinkedList.findNode).to.be.a('function');
     expect(doublyLinkedList.insertAtEnd).to.be.a('function');
     expect(doublyLinkedList.insertBefore).to.be.a('function');
     expect(doublyLinkedList.insertAfter).to.be.a('function');
@@ -19,9 +20,91 @@ describe('Doubly Linked List', function() {
     expect(doublyLinkedList.contains).to.be.a('function');
   });
 
-  // Head and tail should point to the first node that's added
+  it('should return a node', function() {
+    doublyLinkedList.insertAtEnd(5);
+    doublyLinkedList.insertAtEnd(4);
+    doublyLinkedList.addToHead(3);
+    expect(doublyLinkedList.findNode(4).value).to.equal(4);
+    expect(doublyLinkedList.findNode(4).previous.value).to.equal(5);
+  });  
 
-  // When we add a new node to the end of the link, tail should point to this node
+  it('head and tail should point to the first node that\'s added', function() {
+    doublyLinkedList.insertAtEnd(5);
+    expect(doublyLinkedList.head.value).to.equal(5);
+    expect(doublyLinkedList.tail.value).to.equal(5);
+  });
+
+  it('should designate a new tail when new nodes are added', function() {
+    doublyLinkedList.insertAtEnd(4);
+    expect(doublyLinkedList.tail.value).to.equal(4);
+    doublyLinkedList.insertAtEnd(3);
+    expect(doublyLinkedList.tail.value).to.equal(3);
+    doublyLinkedList.insertAtEnd(5);
+    expect(doublyLinkedList.tail.value).to.equal(5);
+  });
+
+  it('head should stay the same when new nodes are added to end', function() {
+    doublyLinkedList.insertAtEnd(4);
+    expect(doublyLinkedList.head.value).to.equal(4);
+    doublyLinkedList.insertAtEnd(5);
+    expect(doublyLinkedList.head.value).to.equal(4);
+  });
+
+  it('should remove the tail from the list when removeTail is called', function() {
+    doublyLinkedList.insertAtEnd(4);
+    doublyLinkedList.insertAtEnd(5);
+    expect(doublyLinkedList.tail.value).to.equal(5);
+    doublyLinkedList.removeTail();
+    expect(doublyLinkedList.tail.value).to.equal(4);
+  });
+
+  // test for add to head
+  it('should update head when node added to head', function() {
+    doublyLinkedList.insertAtEnd(5);
+    doublyLinkedList.insertAtEnd(4);
+    doublyLinkedList.addToHead(3);
+    expect(doublyLinkedList.head.value).to.equal(3);
+    expect(doublyLinkedList.tail.value).to.equal(4);
+  });  
+
+  // test for insert before
+  it('should insert node before another node', function() {
+    doublyLinkedList.insertAtEnd(5);
+    doublyLinkedList.insertAtEnd(4);
+    doublyLinkedList.insertBefore(5, 3);
+    expect(doublyLinkedList.head.value).to.equal(3);
+    expect(doublyLinkedList.tail.value).to.equal(4);
+    expect(doublyLinkedList.contains(5)).to.equal(true);
+  });    
+
+  // test for insert after
+  it('should insert node after another node', function() {
+    doublyLinkedList.insertAtEnd(5);
+    doublyLinkedList.insertAtEnd(4);
+    doublyLinkedList.insertAfter(4, 3);
+    expect(doublyLinkedList.head.value).to.equal(5);
+    expect(doublyLinkedList.tail.value).to.equal(3);
+    expect(doublyLinkedList.contains(4)).to.equal(true);
+  });
+
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- sprint-two/src/doublyLinkedList.js
+++ sprint-two/src/doublyLinkedList.js
@@ -10,22 +10,44 @@ var Node = function(value) {
 };
 
 DoublyLinkedList.prototype.insertAtEnd = function(value) {
-//.insertAtEnd
  //adding first node (.addToTail)
+ 
  //create a node with the value passed in to Node. set value property to value passed in
+  var node = new Node(value);
+ 
+ //if list is empty
+  if (!this.head) {
  // have head make a reference to new node
  // have tail make a reference to new node
-
- //.insertAtEnd
- //create a node with the value passed in to Node. set value property to value passed in
- // have the tail.next point to new node
+    this.head = node;
+    this.tail = node;
+  } else {
+ // list already has a node
  // have new node's previous property make a reference to the tail
+    node.previous = this.tail;
+    this.tail.next = node;
  // reassign tail to newly created node
+    this.tail = node;
+  }
 
 };
 
+DoublyLinkedList.prototype.findNode = function(value, node) {
+  // Start with this.head
+  node = node || this.head;
+  // If passed in node is equal to value...
+  if (node.value === value) {
+  // ...return node
+    return node;
+  } else {
+  // Call findNode on node.next
+    return this.findNode(value, node.next);
+  }
+  // Otherwise return false
+  return false;
+};
 
-//.insertBefore
+DoublyLinkedList.prototype.insertBefore = function(before, value) {
 // Make sure the indicated before node exists.
 // create a node with the value passed in to Node. set value property to value passed in
 // make new node's previous property point to passed in node's previous property
@@ -33,7 +55,10 @@ DoublyLinkedList.prototype.insertAtEnd = function(value) {
 // make passed in node's previous property point to new node
 // make new node's next property point to passed in node
 // if indeicated node is head, invoke .addToHead(new node);
+};
 
+DoublyLinkedList.prototype.insertAfter = function(after, value) {
+};
 //.insertAfter
 // Make sure the indicated after node exists
 // create a node with the value passed in to Node. set value property to value passed in
@@ -43,23 +68,44 @@ DoublyLinkedList.prototype.insertAtEnd = function(value) {
 // point new node's previous property point to passed in node. 
 // if passed in node is tail invoke .insertAtEnd(new node)
 
-
+DoublyLinkedList.prototype.addToHead = function(value) {
+};
 //.addToHead
  //create a node with the value passed in to Node. set value property to value passed in
  //point next to the head
  //point the head's previous to the newly created node
  //redirect head to newly created node.
 
- //.removeTail
- // Save the tail value property in a variable
- // Point tail to tail's previous node
- // Change the new Tail's next property to null
- // Return tail's value variable
+DoublyLinkedList.prototype.removeTail = function() {
+};
+//.removeTail
+// Save the tail value property in a variable
+// Point tail to tail's previous node
+// Change the new Tail's next property to null
+// Return tail's value variable
 
- //.contains
- // Does current node value match target
- // If value equals target, return true
- // Otherwise, check if the node's next property is valid
- // If valid, pass in the nodes's next property into the function
- // Return false
+DoublyLinkedList.prototype.contains = function(value) {
+};
+//.contains
+// Does current node value match target
+// If value equals target, return true
+// Otherwise, check if the node's next property is valid
+// If valid, pass in the nodes's next property into the function
+// Return false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
